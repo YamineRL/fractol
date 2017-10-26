71f65123348660791d3a2e79f7871ff04753d63b
YamineRL <yhaddar@student.42.fr>
2017-10-26 19:34:39 +0200

    typos fixed

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: yhaddar <yhaddar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/25 19:34:36 by yhaddar           #+#    #+#             */
-/*   Updated: 2017/10/26 19:28:41 by yhaddar          ###   ########.fr       */
+/*   Updated: 2017/10/26 19:33:05 by yhaddar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ int			error(int ac, char **av)
 {
 	if (ac != 2)
 		return (0);
-	if (ft_strcmp(av[1], "1") != 0 &&ft_strcmp(av[1], "2") != 0
-			&& ft_strcmp(av[1], "3") != 0 &&ft_strcmp(av[1], "4") != 0
-			&&ft_strcmp(av[1], "5") != 0)
+	if (ft_strcmp(av[1], "1") != 0 && ft_strcmp(av[1], "2") != 0
+			&& ft_strcmp(av[1], "3") != 0 && ft_strcmp(av[1], "4") != 0
+			&& ft_strcmp(av[1], "5") != 0)
 		return (0);
 	return (1);
 }
